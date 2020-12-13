28d191773c5c590ac53b72181993e1f523610b0d
vista1205 <vista35@vtomske.ru>
2020-12-14 00:14:26 +0700

    Всё работает, лоск наведён, осталось сделать авторизацию

--- Lesson7/MainWindow.xaml.cs
+++ Lesson7/MainWindow.xaml.cs
@@ -43,7 +43,6 @@ namespace Lesson7
                 dtDepart = new DataTable();
                 dadapter.Fill(dtDepart);
                 alldepDataGrid.DataContext = dtDepart.DefaultView;
-                allDepComboBox.ItemsSource = dtDepart.DefaultView;
                 connect.Close();
             }
         }
@@ -97,19 +96,30 @@ namespace Lesson7
                 }                
                 if (edtempl.ShowDialog() == true)
                 {
-                    idemployee = Convert.ToInt32(result);                    
-                    string com= $"UPDATE [Employee] SET [ФИО]='{edtempl.FIOtextBox.Text}', [Дата рождения]='{(DateTime)edtempl.edtdatePicker.SelectedDate}', [E-mail]='{edtempl.MailtextBox.Text}', [Департамент]='{edtempl.allDepComboBox1.SelectedIndex+1}', [Телефон]='{edtempl.PhonetextBox.Text}' WHERE [Employee].[ID]='{idemployee}';";
-                    using(SqlConnection connect=new SqlConnection(connectionString))
+                    if (Convert.ToString(edtempl.FIOtextBox.Text) != "" && Convert.ToString(edtempl.allDepComboBox1.SelectedValue) != "")
                     {
-                        connect.Open();
-                        SqlCommand command = new SqlCommand(com, connect);
-                        SqlDataAdapter dataAdapter = new SqlDataAdapter();
-                        dataAdapter.SelectCommand = command;
-                        dataAdapter.Fill(dtGeneral);
-                        connect.Close();
-                        ObnvEmployee();
+                        idemployee = Convert.ToInt32(result);
+                        string com = $"UPDATE [Employee] SET [ФИО]='{edtempl.FIOtextBox.Text}', [Дата рождения]='{(DateTime)edtempl.edtdatePicker.SelectedDate}', [E-mail]='{edtempl.MailtextBox.Text}', [Департамент]='{edtempl.allDepComboBox1.SelectedIndex + 1}', [Телефон]='{edtempl.PhonetextBox.Text}' WHERE [Employee].[ID]='{idemployee}';";
+                        using (SqlConnection connect = new SqlConnection(connectionString))
+                        {
+                            connect.Open();
+                            SqlCommand command = new SqlCommand(com, connect);
+                            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                            dataAdapter.SelectCommand = command;
+                            dataAdapter.Fill(dtGeneral);
+                            connect.Close();
+                            ObnvEmployee();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка! Вы не ввели ФИО сотрудника или не выбрали его Департамент!", "Ошибка Данных!", MessageBoxButton.OK, MessageBoxImage.Error);
                     }
                 }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка! Вы не выбрали работника для изменения!", "Ошибка Данных!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
@@ -119,15 +129,22 @@ namespace Lesson7
             addEmployee.addDepComboBox.ItemsSource = dtDepart.DefaultView;
             if (addEmployee.ShowDialog() == true)
             {
-                string com = $"INSERT INTO [Employee] ([ФИО], [Дата рождения], [E-mail], [Департамент], [Телефон]) VALUES ('{addEmployee.FIOtextBox.Text}', '{(DateTime)addEmployee.edtdatePicker.SelectedDate}', '{addEmployee.MailtextBox.Text}', '{addEmployee.addDepComboBox.SelectedIndex + 1}', '{addEmployee.PhonetextBox.Text}');";
-                using(SqlConnection connect=new SqlConnection(connectionString))
+                if (addEmployee.FIOtextBox.Text != "" && addEmployee.addDepComboBox.SelectedItem != "")
                 {
-                    connect.Open();
-                    SqlCommand command = new SqlCommand(com, connect);
-                    SqlDataAdapter dataAdapter = new SqlDataAdapter();
-                    dataAdapter.SelectCommand = command;
-                    dataAdapter.Fill(dtGeneral);
-                    ObnvEmployee();
+                    string com = $"INSERT INTO [Employee] ([ФИО], [Дата рождения], [E-mail], [Департамент], [Телефон]) VALUES ('{addEmployee.FIOtextBox.Text}', '{(DateTime)addEmployee.edtdatePicker.SelectedDate}', '{addEmployee.MailtextBox.Text}', '{addEmployee.addDepComboBox.SelectedIndex + 1}', '{addEmployee.PhonetextBox.Text}');";
+                    using (SqlConnection connect = new SqlConnection(connectionString))
+                    {
+                        connect.Open();
+                        SqlCommand command = new SqlCommand(com, connect);
+                        SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                        dataAdapter.SelectCommand = command;
+                        dataAdapter.Fill(dtGeneral);
+                        ObnvEmployee();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка! Вы не ввели ФИО нового сотрудника или не выбрали его Департамент!", "Ошибка Данных!", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
         }
@@ -150,18 +167,30 @@ namespace Lesson7
                 int index = Convert.ToInt32(result);
                 if (editDepartment.ShowDialog() == true)
                 {
-                    string com = $"UPDATE [Department] SET [Департамент]='{editDepartment.newName.Text}' WHERE [Department].[ID]='{index}';";
-                    using (SqlConnection connect = new SqlConnection(connectionString))
+                    if (editDepartment.newName.Text != "")
                     {
-                        connect.Open();
-                        SqlCommand command = new SqlCommand(com, connect);
-                        SqlDataAdapter dataAdapter = new SqlDataAdapter();
-                        dataAdapter.SelectCommand = command;
-                        dataAdapter.Fill(dtDepart);
-                        ObnvEmployee();
-                        ObnvDepartment();
+                        string com = $"UPDATE [Department] SET [Департамент]='{editDepartment.newName.Text}' WHERE [Department].[ID]='{index}';";
+                        using (SqlConnection connect = new SqlConnection(connectionString))
+                        {
+                            connect.Open();
+                            SqlCommand command = new SqlCommand(com, connect);
+                            SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                            dataAdapter.SelectCommand = command;
+                            dataAdapter.Fill(dtDepart);
+                            ObnvEmployee();
+                            ObnvDepartment();
+                        }
                     }
+                    else
+                    {
+                        MessageBox.Show("Ошибка! Вы не ввели новое название департамента!", "Ошибка Данных!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                    
                 }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка! Вы не выбрали Отдел для изменения!", "Ошибка Данных!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
@@ -170,15 +199,23 @@ namespace Lesson7
             AddDepartment addDepartment = new AddDepartment();
             if (addDepartment.ShowDialog() == true)
             {
-                using(SqlConnection connect=new SqlConnection(connectionString))
+                if (addDepartment.addDeptextBox.Text != "")
                 {
-                    connect.Open();
-                    string com = $"INSERT INTO [Department] ([Департамент]) VALUES ('{addDepartment.addDeptextBox.Text}');";
-                    SqlCommand command = new SqlCommand(com, connect);
-                    SqlDataAdapter dataAdapter = new SqlDataAdapter();
-                    dataAdapter.SelectCommand = command;
-                    dataAdapter.Fill(dtDepart);
-                    ObnvDepartment();
+                    using (SqlConnection connect = new SqlConnection(connectionString))
+                    {
+                        connect.Open();
+                        string com = $"INSERT INTO [Department] ([Департамент]) VALUES ('{addDepartment.addDeptextBox.Text}');";
+                        SqlCommand command = new SqlCommand(com, connect);
+                        SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                        dataAdapter.SelectCommand = command;
+                        dataAdapter.Fill(dtDepart);
+                        connect.Close();
+                        ObnvDepartment();
+                    }
+                }
+                if(addDepartment.addDeptextBox.Text=="")
+                {
+                    MessageBox.Show("Ошибка! Вы не ввели название нового департамента!", "Ошибка Данных!", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
         }
